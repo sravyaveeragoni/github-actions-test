996be0382ee61e90d7d85f3b535f20e4e4def6c4
Sravya Veeragoni <sravyaveeragoni20@gmail.com>
2026-03-04 12:25:46 -0600

    code

---
+++ github-cicd/src/main/java/com/example/github_cicd/GithubCicdApplication.java
@@ -0,0 +1,21 @@
+package com.example.github_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdApplication {
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	//
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdApplication.class, args);
+	}
+
+}
